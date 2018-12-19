73321799fd7d24c31554bdfadc9b555a75a3e96f
pangjunpeng <pangjunpeng@newstylegroup.com>
2018-12-19 11:11:08 +0800

    简易todolist完成

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,32 +1,61 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd'
 import './TodoList.scss'
 
 class TodoList extends Component {
-    // constructor(props){
-    //     super(props)
-    // }
+    constructor(props){
+        super(props)
+        this.state = {
+            iptValue: '',
+            list: [1,2,3,4,5,6]
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.addItem = this.addItem.bind(this)
+        this.delItem = this.delItem.bind(this)
+    }
+    handleChange(e){
+        this.setState({
+            iptValue: e.target.value
+        })
+    }
+    addItem(){
+        this.setState( () => {
+            return {
+                list: [...this.state.list, this.state.iptValue],
+                iptValue: ''
+            }
+        })
+    }
+    delItem(index){
+        this.setState( () => {
+            this.state.list.splice(index, 1)
+            const list = this.state.list
+            return {
+                list
+            }
+        })
+    }
     render(){
-        const data = [
-            '1',
-            '2'
-        ]
         return (
-            <Fragment>
-                <div>
+            <div className="todo-wrapper">
+                <div className="todo-ipt-wrapper">
                     <Input
                         className="todo-input"
                         placeholder="请添加事项"
+                        value={this.state.iptValue}
+                        onChange={this.handleChange}
                     ></Input>
-                    <Button type="primary" className="todo-btn">添加</Button>
+                    <Button type="primary" className="todo-btn" onClick={this.addItem}>添加</Button>
                 </div>
                 <List
+                    className="todo-list"
                     bordered
-                    dataSource={data}
-                    renderItem={item => (<List.Item>{item}</List.Item>)}
+                    dataSource={this.state.list}
+                    renderItem={(item,index) => (<List.Item onClick={this.delItem.bind(null,index)}>{item}</List.Item>)}
+                    locale={{emptyText: '没有数据了'}}
                 />
-            </Fragment>
+            </div>
         )
     }
 }
